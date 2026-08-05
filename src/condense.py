f0197d8d923b8193f5e2f5e4836fff0ad88188e3
Simone J. Skeen <125575635+sskeen@users.noreply.github.com>
2026-08-05 17:16:18 -0400

    Fit pilot StepMix model

--- src/condense.py
+++ src/condense.py
@@ -21,17 +21,18 @@ def condense(df: pd.DataFrame) -> pd.DataFrame:
     """
     # Excise web metadata & aim ii artifacts
     df = df.drop(columns=[
-        'date',      # datetime in YYY-MM-DD hh:mm:ss
-        'p_au',      # post author
-        'p_utc',     # datetime in Coordinated Universal Time
-        'p_date',    # datetime (dupe)
-        'id',        # Reddit post id
-        'n_cmnt',    # number of top-level comments
-        'not_us',    # 0/1 for mentions of non-U.S. geographies
-        'cpnd_pred', # computed indicator for comorbid asp + dep + val
-        'Segment',   # concatenation artifact
-        'tech',      # LIWC-encoded placebo outcome
-        'tech_high', # placebo outcome: quantile split
+        'date',      ### datetime in YYY-MM-DD hh:mm:ss
+        'p_au',      ### post author
+        'p_utc',     ### datetime in Coordinated Universal Time
+        'p_date',    ### datetime (dupe)
+        'id',        ### Reddit post id
+        'n_cmnt',    ### number of top-level comments
+        'not_us',    ### 0/1 for mentions of non-U.S. geographies
+        'sui_re',    ### explicit suicidality regex max: pre-disambiguation
+        'cpnd_pred', ### computed indicator for comorbid asp + dep + val
+        'Segment',   ### concatenation artifact
+        'tech',      ### LIWC-encoded placebo outcome
+        'tech_high', ### placebo outcome: quantile split
     ])
 
     # Ditch deprecated `p_` prefix
@@ -47,5 +48,8 @@ def condense(df: pd.DataFrame) -> pd.DataFrame:
     prob_cols = [col for col in df.columns if col.endswith('_prob')]
     for col in prob_cols:
         df[col] = df[col].apply(lambda x: round(ast.literal_eval(x)[1], 2))
+
+    # Drop '_pred' suffix from column names
+    df = df.rename(columns={col: col.removesuffix('_pred') for col in df.columns if col.endswith('_pred')})
 
     return df
